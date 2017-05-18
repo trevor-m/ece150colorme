5d207d962b43862caa8000a6a7db840aae7142c5
unknown <trevorm@sbcglobal.net>
2017-05-17 23:13:35 -0700

    Initial commit

---
+++ app/src/main/java/ece150/colorme/MainActivity.java
@@ -0,0 +1,61 @@
+package ece150.colorme;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Fragment fragment;
+    private FragmentManager fragmentManager;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            // Determine which fragment to switch to
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new HomeFragment();
+                    break;
+                case R.id.navigation_addphotos:
+                    fragment = new ImportFragment();
+                    break;
+                case R.id.navigation_gallery:
+                    // TODO
+                    //fragment = new GalleryFragment();
+                    break;
+            }
+
+            // Swap out content with fragment
+            final FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.replace(R.id.content, fragment).commit();
+            return true;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Set up fragments
+        fragmentManager = getSupportFragmentManager();
+        // Start at home fragment always
+        fragment = new HomeFragment();
+        final FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(R.id.content, fragment).commit();
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
